dd339c6b00dcb9e5a63c7b3449fd74d70352a34e
Antonio Sucre <ajs2104@gmail.com>
2020-12-26 20:31:22 -0300

    Adding UpdateTodoCommand

--- Todo.Domain/Commands/UpdateTodoCommand.cs
+++ Todo.Domain/Commands/UpdateTodoCommand.cs
@@ -5,21 +5,24 @@ using Todo.Domain.Commands.Contracts;
 
 namespace Todo.Domain.Commands
 {
-    public class MarkTodoAsUndoneCommand : Notifiable, ICommand
+    public class UpdateTodoCommand : Notifiable, ICommand
     {
-        public MarkTodoAsUndoneCommand() { }
-        public MarkTodoAsUndoneCommand(Guid id, string user)
+        public UpdateTodoCommand() { }
+        public UpdateTodoCommand(Guid id, string title, string user)
         {
             Id = id;
+            Title = title;
             User = user;
         }
 
         public Guid Id { get; set; }
+        public string Title { get; set; }
         public string User { get; set; }
         public void Validate()
         {
             AddNotifications(
                 new Contract()
+                .HasMinLen(Title, 3, "Title", "Por favor, descreve melhor a tarefa")
                 .HasMinLen(User, 6, "User", "Usuario inválido")
             );
         }
